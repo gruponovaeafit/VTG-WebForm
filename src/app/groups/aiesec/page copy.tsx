884770abd5a2dbdd96bd09206c3e0694fef758a5
NoprauxX12 <juanselizca@outlook.com>
2025-12-23 11:36:33 -0500

    UPDATE | Cookie placed in Aiesec form

--- src/app/groups/aiesec/page copy.tsx
+++ src/app/groups/aiesec/page copy.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import Image from "next/image";
 import PixelsAnimation from "../../globalcomponents/UI/Pixels_animation";
-import AiesecForm from "../../globalcomponents/Forms/Form-Aiesec";
 import { useEffect } from "react";
-import Footer from "@/app/globalcomponents/UI/Footer";
 import { useRouter } from "next/navigation";
+import Clubin1Form from "@/app/globalcomponents/Forms/Form-Clubin";
+import Footer from "@/app/globalcomponents/UI/Footer";
 
 export default function Home() {
   const router = useRouter();
@@ -28,30 +29,33 @@ export default function Home() {
     };
   }, [router]);
 
-  
   return (
     <div
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-white"
-      style={{
-        backgroundImage: "url('/aiesec.svg')",
-        backgroundSize: "cover",
-        position: "relative",
-        overflow: "hidden",
-      }}
+    className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-white"
+    style={{
+      backgroundImage: "url('https://novaeafit.blob.core.windows.net/vtg-2025-1/clubin.svg')",
+      backgroundSize: "cover",
+      position: "relative",
+      overflow: "hidden",
+    }}
     >
-      <div style={{ pointerEvents: "none" }}>
-        <PixelsAnimation />
-      </div>
+        <div style={{ pointerEvents: "none" }}>
+          { <PixelsAnimation /> }
+        </div>  
 
+      {/* Contenido principal */}
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start relative z-10 ">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center pixel-font text-white glitch_aiesec">
-          AIESEC
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center mb-6 pixel-font text-white pulse">
+          CLUBIN
         </h1>
 
-        <AiesecForm />
+        <Clubin1Form />
 
         <Footer />
-      </main>
-    </div>
+
+      </main>  
+      
+    </div>    
+
   );
 }
