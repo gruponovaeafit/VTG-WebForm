7f5195e57d8deae7d585bc2ac039672fc2c3a1c1
JANDRESMh2 <158773979+JANDRESMh2@users.noreply.github.com>
2026-01-21 15:09:00 -0500

    vista de clubin y clubmerc

--- src/app/groups/clubin/page.tsx
+++ src/app/groups/clubin/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import ClubinForm from "../../globalcomponents/Forms/Form-Clubin";
 import Image from "next/image";
 import PixelsAnimation from "../../globalcomponents/UI/Pixels_animation";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Clubin1Form from "@/app/globalcomponents/Forms/Form-Clubin";
 import Footer from "@/app/globalcomponents/UI/Footer";
+import ConfettiAnimation from "@/app/globalcomponents/UI/ConfettiAnimation";
 
 export default function Home() {
   const router = useRouter();
@@ -34,23 +36,42 @@ export default function Home() {
     <div
     className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-white"
     style={{
-      backgroundImage: "url('/clubin.svg')",
+      backgroundImage: "url('/CLUBIN_Solid.svg')",
       backgroundSize: "cover",
       position: "relative",
       overflow: "hidden",
     }}
     >
+     <img
+  src="/CLUBIN_E1.svg"
+  alt=""
+  aria-hidden="true"
+  className="
+    pointer-events-none select-none absolute top-0 right-0 -translate-y-1/4 translate-x-1/4
+    z-0 w-[45vw] max-w-[260px]
+  "
+/>
+      <img
+  src="/CLUBIN_E2.svg"
+  alt=""
+  aria-hidden="true"
+  className="
+    pointer-events-none select-none absolute bottom-0 left-0  
+    z-0 w-[55vw] max-w-[380px]
+  "
+/>
+
         <div style={{ pointerEvents: "none" }}>
-          { <PixelsAnimation /> }
+          { <ConfettiAnimation /> }
         </div>  
 
       {/* Contenido principal */}
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start relative z-10 ">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center mb-6 pixel-font text-white pulse">
-          CLUBIN
-        </h1>
+      <main className="flex flex-col gap-0 row-start-2 items-center sm:items-start relative z-10 font-extrabold font-extrabold mb-2 ">
+        <img src="/CLUBIN.svg" alt="CLUBIN" width={150} height={120} />
 
-        <Clubin1Form />
+        <div className="-mt-5">
+          <Clubin1Form />
+        </div>
 
         <Footer />
 
--- src/app/groups/clubmerc/page.tsx
+++ src/app/groups/clubmerc/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+
 import Footer from "@/app/globalcomponents/UI/Footer";
 import PixelsAnimation from "../../globalcomponents/UI/Pixels_animation";
 import ClubmercForm from "../../globalcomponents/Forms/Form-Clubmerc";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import ConfettiAnimation from "@/app/globalcomponents/UI/ConfettiAnimation";
 
 export default function Home() {
   const router = useRouter();
@@ -31,30 +33,49 @@ export default function Home() {
 
 
   return (
+    
     <div
     className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-white"
     style={{
-      backgroundImage: "url('/clubmerc.svg')",
+      backgroundImage: "url('/CLUBMERC_Solid.svg')",
       backgroundSize: "cover",
       position: "relative",
       overflow: "hidden",
     }}
     >
+      <img
+  src="/CLUBMERC_E1.svg"
+  alt=""
+  aria-hidden="true"
+  className="
+    pointer-events-none select-none absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/4
+    z-0 w-[120%] max-w-none
+  "
+/>
+
+<img
+  src="/CLUBMERC_E2.svg"
+  alt=""
+  aria-hidden="true"
+  className="
+    pointer-events-none select-none absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/4
+    z-0 w-[120%] max-w-none
+  "
+/>
+
         <div style={{ pointerEvents: "none" }}>
-          <PixelsAnimation />
+          { <ConfettiAnimation /> }
         </div>
 
       {/* Contenido principal */}
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start relative z-10 ">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center mb-6 pixel-font text-white glitch_clubmerc">
-          CLUBMERC
-        </h1>
+        <img src="/CLUBMERC.svg" alt="CLUBMERC" width={150} height={120} />
 
-        <ClubmercForm />
+        <div className="-mt-5">
+          <ClubmercForm/>
+        </div>
 
         <Footer/>
-        
-
       </main>
 
     </div>
